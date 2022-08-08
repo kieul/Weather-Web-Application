d43d95a73e7beb2aec999f2268ac24365c19e67b
kieul <kieu.thuy.linh1@gmail.com>
2022-08-08 14:55:57 +0200

    Created html, css, js file

---
+++ src/javascript.js
@@ -0,0 +1,80 @@
+//current time
+let now = new Date();
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+//from celsius to fahrenheit
+function showWeather(response) {
+  document.querySelector("#displayed-city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+function search(city) {
+  let apiKey = "7fef5d169773c3ea2391e9aa3ffecb34";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeather);
+}
+function searchCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  search(city);
+}
+function searchCurrentLocation(position) {
+  let apiKey = "7fef5d169773c3ea2391e9aa3ffecb34";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showWeather);
+}
+
+function currentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchCurrentLocation);
+}
+function convertFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 75;
+}
+function convertCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 24;
+}
+
+let day = days[now.getDay()];
+
+let currentDay = document.querySelector("#current-time");
+currentDay.innerHTML = `${day} ${hours}:${minutes}`;
+
+let CurrentLocationButton = document.querySelector("#current-location");
+CurrentLocationButton.addEventListener("click", currentLocation);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", convertFahrenheit);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", convertCelsius);
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", searchCity);
+
+search("NeW York");
